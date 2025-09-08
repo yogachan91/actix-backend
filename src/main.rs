12b060870184bc39c2edfb3bd86b71f3852ccf55
Yoga Chandra Widjaya <yogachandraw@gmail.com>
2025-09-09 02:26:10 +0700

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(redis_client.clone()))
             .configure(routes::config)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
